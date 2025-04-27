1d230e60d4de8ea61ce0c6e6b1b7c71cb4e2484a
Light Celline <166616850+lightceline@users.noreply.github.com>
2025-04-28 02:03:11 +0700

    Delete Coursework directory

--- Coursework/Coursework/includes/DatabaseConnection.php
+++
@@ -1,8 +0,0 @@
-<?php
-try {
-    $pdo = new PDO('mysql:host=localhost;dbname=light;charset=utf8mb4', 'root', '');
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch (PDOException $e) {
-    die("Connection failed: " . $e->getMessage());
-}
-?>
--- Coursework/Coursework/templates/home.html.php
+++
@@ -1,2 +0,0 @@
-<?php
-include '../templates/post.html.php';
